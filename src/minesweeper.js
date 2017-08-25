ba549146d5cb8ec361c30e5f5715fd9ba8eeef9a
mdevlin14 <mdevlin14@gmail.com>
2017-08-25 00:59:12 -0500

    dynamic game and bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,45 @@
-const printBoard = board => {
-  console.log('Current board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for ( let i = 0; i < numberOfRows; i++ ) {
+    let row = [];
+    for ( let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+  };
+
+//console.log(generatePlayerBoard(3, 3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for ( let i = 0; i < numberOfRows; i++ ) {
+    let row = [];
+    for ( let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    // potential to place bombs on top of existing bombs. Learn control flow.
+  }
+  return board;
 };
 
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
 
-printBoard(board);
+console.log('Player Board: ');
+printBoard(playerBoard);
 
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
