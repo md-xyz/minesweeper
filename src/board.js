20ac3119031847a1eaa741ff6c426fee6d03ecc8
mdevlin14 <mdevlin14@gmail.com>
2017-09-08 16:09:22 -0500

    final code

--- src/board.js
+++ src/board.js
@@ -1,22 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('You lose. That\'s a bomb!');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()) {
-      console.log('Yay. you won by default');
-    } else {
-      console.log('Current board: ');
-      this._board.print();
-    }
-  }
-}
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs){
     // this._numberOfRows = numberOfRows;
     // this._numberOfColumns = numberOfColumns;
@@ -94,6 +76,3 @@ class Board {
       return board;
     }
 }
-
-const g = new Game(3, 3, 3);
-g.playMove(1, 1);
